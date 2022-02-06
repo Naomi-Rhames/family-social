15d970182a81a51a35217fbf7c5cbe19affca6b1
Naomi-Rhames <naomirhames2020@gmail.com>
2022-02-05 20:59:07 -0600

    User Auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-// import './App.css';
-import { Auth } from './components'
+ import './App.css';
+ import { Auth } from './components'
+// import { Auth, EventFeed} from './components'
+// import { connect } from 'react-redux'
+// import { useEffect } from 'react'
+// import { Switch, Route} from 'react-router';
 
 function App() {
+ 
   return (
-    <>
+ <>
     <h1>Welcome to Family Social Web application</h1>
-      <div> <Auth/></div>
-    </>
-
+      <Auth/>
+ </>
   );
-}
+  
+  }
 
-export default App;
+// }
+// {/* const mapStateToProps = (state) => ({user: state.user}) */}
+
+  export default App;
+
